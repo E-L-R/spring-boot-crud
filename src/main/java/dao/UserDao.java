ec4829349588fea93eccc344e875ca8661a813eb
E-L-R <132351158+E-L-R@users.noreply.github.com>
2026-07-26 13:04:06 +0900

    feat(dao): implement User DAO interface and implementation

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,12 @@
 package dao;
 
-public class UserDao {
+import model.User;
+import java.util.List;
+
+public interface UserDao {
+    List<User> findAll();
+    void save(User user);
+    User findById(Long id);
+    void update(User user);
+    void delete(Long id);
 }
