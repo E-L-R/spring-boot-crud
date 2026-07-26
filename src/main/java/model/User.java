124725b47ce546186ee547693c407e1485f064d2
E-L-R <132351158+E-L-R@users.noreply.github.com>
2026-07-26 13:01:34 +0900

    feat(model): implement User entity class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,32 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
 }
