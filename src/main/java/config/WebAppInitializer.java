2d4357762bb59d46abed5b7b7946a876281f5aa5
E-L-R <132351158+E-L-R@users.noreply.github.com>
2026-07-26 12:58:46 +0900

    feat(config): implement configuration classes

--- src/main/java/config/WebAppInitializer.java
+++ src/main/java/config/WebAppInitializer.java
@@ -1,4 +1,32 @@
 package config;
 
-public class WebAppInitializer {
+import jakarta.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; // Map DispatcherServlet to the root URL
+    }
+
+    // Ensures we don't get weird character rendering for non-English data in Thymeleaf/MySQL
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return new Filter[]{encodingFilter};
+    }
 }
