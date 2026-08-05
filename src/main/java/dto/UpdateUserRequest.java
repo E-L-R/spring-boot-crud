14108af549ee9227eeaf3f9d6b07e197cde7f788
E-L-R <132351158+E-L-R@users.noreply.github.com>
2026-08-05 13:25:28 +0900

    feat(dto): modify dto for security

--- src/main/java/dto/UpdateUserRequest.java
+++ src/main/java/dto/UpdateUserRequest.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateUserRequest {
+    private String username;
+    private String password;
     private String firstName;
     private String lastName;
     private String email;
     private Integer age;
+    private List<String> roles;
 }
